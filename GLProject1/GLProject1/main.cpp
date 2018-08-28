90925da3ab8c383b4d20518288232c235e62da0f
达尼 <hushiyu@shangyoo.com>
2018-08-28 15:29:31 +0800

    feat : 绘制矩形

--- GLProject1/GLProject1/main.cpp
+++ GLProject1/GLProject1/main.cpp
@@ -125,6 +125,30 @@ int main()
 	glDeleteShader(vertShader);
 	glDeleteShader(fragmentShader);
 
+	GLuint VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	//绑定VAO
+	glBindVertexArray(VAO);
+
+	//顶点数据放入VBO中
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//索引放入EBO中
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//解绑VAO
+	glBindVertexArray(0);
+
 	// Game loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -136,9 +160,18 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Swap the screen buffers
+		//绘制三角形
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		// Swap the screen buffers(双缓冲相关)
 		glfwSwapBuffers(window);
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	// Terminate GLFW, clearing any resources allocated by GLFW.
 	glfwTerminate();
